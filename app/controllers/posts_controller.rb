4e3745bc3587ff49294f979964ef36fbbc89d29c
Scott C. Reynolds <scott@scottcreynolds.com>
2016-02-21 20:59:21 -0500

    solution

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,44 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update]
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(post_params)
+    @post.save
+    redirect_to post_path(@post)
+  end
+
+  def edit
+  end
+
+  def update
+    @post.update(post_params)
+    redirect_to post_path(@post)
+  end
+
+  def post_data
+    post = Post.find(params[:id])
+    render json: PostSerializer.serialize(post)
+  end
+
+private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def post_params
+    params.require(:post).permit(:title, :description)
+  end
+end
