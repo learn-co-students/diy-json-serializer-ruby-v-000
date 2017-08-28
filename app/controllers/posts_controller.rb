3cc005b2a07c473e2987281d520dbba5e22b2874
sarjumulmi <mulmi.sarju@gmail.com>
2017-08-27 21:22:57 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,6 +18,11 @@ class PostsController < ApplicationController
     redirect_to post_path(@post)
   end
 
+  # def body
+  #   post = Post.find(params[:id])
+  #   render json: PostSerializer.serialize(post)
+  # end
+
   def edit
   end
 
@@ -28,7 +33,7 @@ class PostsController < ApplicationController
 
   def post_data
     post = Post.find(params[:id])
-    render plain: post.description
+    render json: PostSerializer.serialize(post)
   end
 
 private
