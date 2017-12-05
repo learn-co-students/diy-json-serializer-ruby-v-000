2aa6528bae1d8ae2eb30b873072a5d9b2f659d6a
Sophia Hubeny <shhubeny@gmail.com>
2017-12-05 08:30:13 +0000

    Done.

---
+++ app/serializers/post_serializer.rb
@@ -0,0 +1,17 @@
+class PostSerializer
+  def self.serialize(post)
+
+    #start with open brace to create JSON
+    serialized_post = '{'
+
+    serialized_post += '"id": ' + post.id.to_s + ', '
+    serialized_post += '"title": "' + post.title + '", '
+    serialized_post += '"description": "' + post.description + '", '
+
+    #author assication can also be represented in JSON
+    serialized_post += '"author": { '
+    serialized_post += '"name": "' + post.author.name + '"}'
+
+    #end with close brace
+    serialized_post += '}'
+end
