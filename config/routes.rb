3538fdfea5e34f48edc0f2dd54ae85b3259488cb
smithwebtek <brad@smithwebtek.com>
2018-03-28 16:15:13 -0400

    Update my version

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # resources :posts, only: [:index, :show, :new, :create, :edit]
-  resources :posts
+	resources :posts
+	get 'posts/:id', to: 'posts#show'
   get 'posts/:id/post_data', to: 'posts#post_data'
   get 'posts/:id/body', to: 'posts#body'
 end
