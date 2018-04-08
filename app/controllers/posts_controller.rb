6b96cd281e9cb777357ab2ee5fcf0006d1ee8861
BenB <bblkmn5@gmail.com>
2018-04-08 16:44:28 -0700

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,11 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def post_data
+    post = Post.find(params[:id])
+    render json: PostSerializer.serialize(post)
+  end
+
   def show
   end
 
