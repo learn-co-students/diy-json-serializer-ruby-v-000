2877a89a7d06e9204c085272c678bc20e7d9ba3b
cmlugoce <cmlugoce@syr.edu>
2018-09-09 12:10:25 -0700

    Done.

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,11 +1,15 @@
 class PostSerializer
-    def self.serializer(post)
-     serialized_post = '{'
-      serialized_post += '"id":' + post.id.to_s + ','
-     serialized_post += '"title": "' + post.title + '",'
-     serialized_post += '"description": "' + post.description + '",'
-      serialized_post += '"author": {'
-     serialized_post += '"name": "' + post.author.name + '"}'
-      serialized_post += '}'
-    end
+  def self.serialize(post)
+    
+    
+    serialized_post = '{'
+     serialized_post += '"id": ' + post.id.to_s + ', '
+    serialized_post += '"title": "' + post.title + '", '
+    serialized_post += '"description": "' + post.description + '", '
+     # the author association can also be represented in JSON
+    serialized_post += '"author": {'
+    serialized_post += '"name": "' + post.author.name + '"}'
+     
+    serialized_post += '}'
   end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :posts, only: [:index, :show, :new, :create, :edit]
   get 'posts/:id/post_data', to: 'posts#post_data'
+  patch 'posts/:id' => 'posts#update'
 end
