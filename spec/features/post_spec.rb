f2f4fa65b2cb89bd292d2b7f97708156d44d6fd2
github username <github email address>
2018-09-18 15:19:46 -0400

    Done.

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -2,10 +2,12 @@ require 'rails_helper'
 
 describe 'navigate' do
   before do
+    # @post = Post.create(title: "My Post", description: "My post desc", name: "Steven")
     @post = Post.create(title: "My Post", description: "My post desc", author_id: 1)
   end
 
   it 'shows the title on the show page in a h1 tag' do
+
     visit post_path(@post)
     expect(page).to have_css("h1", text: "My Post")
   end
@@ -16,21 +18,23 @@ describe 'navigate' do
   end
 
   it 'shows the description on the show page in a p tag' do
-
     visit post_path(@post)
-
     expect(page).to have_css("p", text: "My post desc")
-
   end
+end
 
 describe 'form' do
   it 'shows a new form that submits content and redirects and prints out params' do
     visit new_post_path
 
-    fill_in 'title', with: "My post title"
-    fill_in 'description', with: "My post description"
+# fill_in 'title', with: "My post title"
+# fill_in 'description', with: "My post description"
 
-    click_on "Submit Post"
+    fill_in 'post_title', with: "My post title"
+    fill_in 'post_description', with: "My post description"
+
+# click_on "Submit Post"
+    click_on "Create Post"
 
     expect(page).to have_content("My post title")
   end
@@ -47,3 +51,4 @@ describe 'form' do
 
     expect(page).to have_content("My edit")
   end
+end
