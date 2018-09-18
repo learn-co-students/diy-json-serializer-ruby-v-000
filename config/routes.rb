dd4a8f5f1f4bbf18cc629272c1e2629fae2c292c
github username <github email address>
2018-09-18 17:54:39 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :posts, only: [:index, :show, :new, :create, :edit]
+  resources :posts, only: [:index, :show, :new, :create, :edit, :update]
   get 'posts/:id/post_data', to: 'posts#post_data'
 end
