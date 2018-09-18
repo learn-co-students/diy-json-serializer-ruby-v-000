f2731782f2243420d48b79d58a27ae6ffc808d51
Kim Gonzales <Kimgo589@gmail.com>
2018-09-18 13:34:35 -0400

    Done.

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -8,9 +8,3 @@
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
-
-require 'rails_helper'
-
-RSpec.describe Author, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
