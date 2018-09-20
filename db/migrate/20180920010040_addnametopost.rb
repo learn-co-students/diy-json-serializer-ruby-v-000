a13dc5571db4b06d0325ce7dc21eb1f4e711fcc6
Sharmina Akhtar <sakht318@gmail.com>
2018-09-20 01:06:19 +0000

    Done.

---
+++ db/migrate/20180920010040_addnametopost.rb
@@ -0,0 +1,5 @@
+class Addnametopost < ActiveRecord::Migration
+  def change
+    add_column :posts, :name, :string
+  end
+end
--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -25,8 +25,8 @@ describe 'form' do
   it 'shows a new form that submits content and redirects and prints out params' do
     visit new_post_path
 
-    fill_in 'title', with: "My post title"
-    fill_in 'description', with: "My post description"
+    fill_in 'post[title]', with: "My post title"
+    fill_in 'post[description]', with: "My post description"
 
     click_on "Submit Post"
 
