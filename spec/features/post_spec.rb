3998859335e65890e3f03ccf93a20d58f4f5161f
cjwood032 <cjwood032@gmail.com>
2018-10-01 20:53:41 -0400

    Done.

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 describe 'navigate' do
   before do
-    @post = Post.create(title: "My Post", description: "My post desc", name: "Steven")
-    binding.pry
+    @post = Post.create(title: "My Post", description: "My post desc")
+    #binding.pry
   end
 
   it 'shows the title on the show page in a h1 tag' do
