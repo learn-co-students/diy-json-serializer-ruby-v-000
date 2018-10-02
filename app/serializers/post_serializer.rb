e51c7cb6d81c9f473103d1a495654a9cae109bf8
github username <github email address>
2018-10-02 11:06:31 -0400

    Done.

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,7 +1,7 @@
 class PostSerializer
   def self.serialize(post)
     serialized_post = '{'
-    serialized_post += '"id"' + post.id.to_s + ','
+    serialized_post += '"id": ' + post.id.to_s + ','
     serialized_post += '"title": "' + post.title + '",'
     serialized_post += '"description": "' + post.description + '",'
     serialized_post += '"author": {'
