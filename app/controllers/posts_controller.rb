940c17b8ca3827fb8e8316ab2f9e94eb874b3187
Amanda <amandarebecca89@gmail.com>
2018-10-16 13:59:52 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -28,7 +28,12 @@ class PostsController < ApplicationController
 
   def post_data
     post = Post.find(params[:id])
-    render plain: post.description
+    render json: PostSerializer.serialize(post)
+  end
+
+  def body
+    post = Post.find(params[:id])
+    render json: PostSerializer.serialize(post)
   end
 
 private
