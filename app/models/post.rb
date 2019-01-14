25e4e14a18f9b03c97e71297f1079d34f6e0c5b9
micahshute <micah.shute@gmail.com>
2019-01-14 11:38:40 -0600

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -13,4 +13,12 @@
 class Post < ActiveRecord::Base
   belongs_to :author
 
+  def name=(name)
+    author = Author.find_or_create_by(name: name)
+    self.author = author
+  end
+
+  def name
+    self.author.name unless self.author.nil?
+  end
 end
