3b32726230f9aba854f7f306339b6da9476b5abb
github username <github email address>
2019-01-25 14:59:29 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,14 +21,9 @@ class PostsController < ApplicationController
   def edit
   end
 
-  def update
-    @post.update(post_params)
-    redirect_to post_path(@post)
-  end
-
   def post_data
     post = Post.find(params[:id])
-    render plain: post.description
+    render json: PostSerializer.serialize(post)
   end
 
 private
