f4edea8b80d080de22c9e721e2a5d9cf25d9f520
Hillevi Eklöw <hillevieklow@gmail.com>
2019-01-28 21:54:48 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,14 +21,14 @@ class PostsController < ApplicationController
   def edit
   end
 
-  def update
-    @post.update(post_params)
-    redirect_to post_path(@post)
-  end
-
   def post_data
     post = Post.find(params[:id])
-    render plain: post.description
+    render json: PostSerializer.serialize(post)
+  end
+
+  def body
+    post = Post.find(params[:id])
+    render json: PostSerializer.serialize(post)
   end
 
 private
@@ -39,6 +39,6 @@ private
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def post_params
-    params.require(:post).permit(:title, :description)
+    params.require(:post).permit(:title, :description, :name)
   end
 end
