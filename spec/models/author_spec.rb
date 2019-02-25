fa63eab8466b4df22e6af1b35cb05fb517c55c6e
Yazgylych Rahymov <y.rahymov@gmail.com>
2019-02-25 08:11:18 -0500

    Done.

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -7,10 +7,10 @@
 #  hometown   :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+# #
 #
-
-require 'rails_helper'
-
-RSpec.describe Author, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+# require 'rails_helper'
+#
+# RSpec.describe Author, type: :model do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
