e098ed45da3389a45ec7901bf68ddcfd24a27dd2
Lucy Wall <lhwall@gmail.com>
2019-02-27 20:00:14 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,6 +16,7 @@ class PostsController < ApplicationController
       end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :posts, only: [:index, :show, :new, :create, :edit]
+  resources :posts
   get 'posts/:id/body', to: 'posts#body'
   get 'posts/:id/post_data', to: 'posts#post_data'
 end
