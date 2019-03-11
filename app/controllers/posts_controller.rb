ad15d509c59f1abefdfbf79d3d0c7be13e532a33
freeWillee <will.ik.lee@gmail.com>
2019-03-10 21:45:01 -0600

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,6 +8,11 @@ class PostsController < ApplicationController
   def show
   end
 
+  def post_data
+    post = Post.find(params[:id])
+    render json: PostSerializer.serialize(post)
+  end
+
   def new
     @post = Post.new
   end
@@ -24,11 +29,6 @@ class PostsController < ApplicationController
   def update
     @post.update(post_params)
     redirect_to post_path(@post)
-  end
-
-  def post_data
-    post = Post.find(params[:id])
-    render plain: post.description
   end
 
 private
--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -9,8 +9,8 @@
 #  updated_at :datetime         not null
 #
 
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe Author, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+# RSpec.describe Author, type: :model do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
