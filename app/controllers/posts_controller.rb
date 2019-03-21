6bbce76071728ac6d935a42cb0c59e4585153852
github username <github email address>
2019-03-21 16:49:43 -0400

    Code-along done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,9 +27,10 @@ class PostsController < ApplicationController
   end
 
   def post_data
-    post = Post.find(params[:id])
-    render plain: post.description
+    post = set_post
+    render json: PostSerializer.serialize(post)
   end
+
 
 private
   # Use callbacks to share common setup or constraints between actions.
