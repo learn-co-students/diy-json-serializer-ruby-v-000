a773c225cb5cf96fb79dff30fe1914d517ca8e76
Neil Seejoor <nvseejoor@gmail.com>
2019-04-08 21:07:44 +0000

    Done.

--- db/migrate/20151230232412_create_posts.rb
+++ db/migrate/20151230232412_create_posts.rb
@@ -3,7 +3,6 @@ class CreatePosts < ActiveRecord::Migration
     create_table :posts do |t|
       t.string :title
       t.text :description
-      t.string :name
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -22,7 +22,6 @@ ActiveRecord::Schema.define(version: 20160119185429) do
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.string   "name"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "author_id"
