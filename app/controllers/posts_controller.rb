0c7af460ab422731c4d8692c915960e503131cce
Naomi-Dennis <njdennis94@gmail.com>
2019-04-26 13:53:51 -0400

    Done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,6 +6,7 @@ class PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
@@ -30,7 +31,10 @@ class PostsController < ApplicationController
     post = Post.find(params[:id])
     render plain: post.description
   end
-
+  def body
+    post = Post.find(params[:id])
+    render json: PostSerializer.serialize(post)
+  end
 private
   # Use callbacks to share common setup or constraints between actions.
   def set_post
