839bc29b7fd677a6a1968d9d06ead781a139df37
github username <github email address>
2019-07-19 19:53:28 -0400

    Done.

--- Gemfile
+++ Gemfile
@@ -37,6 +37,8 @@ group :development, :test do
   gem 'capybara'
   gem 'database_cleaner'
   gem 'annotate'
+  gem 'pry'
+  gem 'rb-readline'
 end
 
 group :development do
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,6 +6,7 @@ class PostsController < ApplicationController
   end
 
   def show
+    
   end
 
   def new
@@ -28,7 +29,7 @@ class PostsController < ApplicationController
 
   def post_data
     post = Post.find(params[:id])
-    render json: post.description
+    render json: PostSerializer.serialize(post)
   end
 
   def body
