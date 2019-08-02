955b9fbd34419d32141c4ec5013d7be3a2cdf548
github username <github email address>
2019-08-02 11:32:36 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -31,6 +31,11 @@ class PostsController < ApplicationController
     render plain: post.description
   end
 
+  def body
+    post = Post.find(params[:id])
+    render plain: post.description
+  end
+
 private
   # Use callbacks to share common setup or constraints between actions.
   def set_post
