2170a957048588cdccd3fc350927ca3b43c2483e
github username <github email address>
2019-08-04 16:11:12 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,11 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def body
+    post = Post.find(params[:id])
+    render plain: post.description
+  end
+
   def show
   end
 
