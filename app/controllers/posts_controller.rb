b3d99694ef71a58c6894d5f4705a6c7a82e35fac
sjwakean <swakeman@ca.rr.com>
2019-08-10 14:05:36 -0700

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,12 +26,6 @@ class PostsController < ApplicationController
     redirect_to post_path(@post)
   end
 
-  def post_data
-    post = Post.find(params[:id])
-    #render plain: post.description #Originally had
-    render json: post #Changed to
-  end
-
   def body
     post = Post.find(params[:id])
     render plain: post.description
