638190b83160a1c5a5c0628c0c3409aa38ffcf9f
Sarah Paz <sarah.paz@gmail.com>
2019-09-08 11:09:06 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,11 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update]
 
+  # def body
+  #   post = Post.find(params[:id])
+  #   render plain: post.description
+  # end
+
   def index
     @posts = Post.all
   end
